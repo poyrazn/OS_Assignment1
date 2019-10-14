1da53d3d6da49de9322d8e22cf7ce6f103f0c964
poyrazn <poyrazf@mef.edu.tr>
2019-10-14 13:57:02 +0300

    Create ipc.h
    reformatting

---
+++ IPC/ipc.h
@@ -0,0 +1,17 @@
+//
+//  ipc.h
+//  OS_Assignment1
+//
+//  Created by Nehir Poyraz on 8.03.2018.
+//  Copyright © 2018 Nehir Poyraz. All rights reserved.
+//
+
+#ifndef ipc_h
+#define ipc_h
+int part1(void);
+void Collatz(int n);
+int part2(void);
+char* toggleCase(char *msg);
+void printStat(int partnum, int processnum, int role);
+
+#endif /* ipc_h */
